23c0dc3588283e490c3b0e89e4955502f19cb820
Musthafa Faishal <72973311+Musshal@users.noreply.github.com>
2020-10-17 17:27:55 +0700

    Add gotham_city.c
    Solved

---
+++ easy/gotham_city.c
@@ -0,0 +1,24 @@
+/* problem solver: mussal */
+
+#include <stdio.h>
+
+int main()
+{
+  /* dictionary */
+  int criminals;
+
+  /* algorithm */
+  scanf("%d", &c);
+
+  if (criminals>0 && criminals<5){
+    printf("I got this!");
+  }
+  else if (criminals>5 && criminals<10){
+    printf("Help me Batman");
+  }
+  else{
+    printf("Good Luck out there!");
+  }
+
+  return 0;
+}
