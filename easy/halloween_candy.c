aeb85352ba80667b74a5e86d4b122c9fcc2fa8b2
Musthafa Faishal <72973311+Musshal@users.noreply.github.com>
2020-10-17 17:24:02 +0700

    Add halloween_candy.c
    Solved

---
+++ easy/halloween_candy.c
@@ -0,0 +1,17 @@
+/* problem solver: musshal */
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  /* dictionary */
+  int houses;
+
+  /* main program */
+  scanf("%d", &houses);
+
+  printf("%0.f", ceil(200.0/houses));
+
+  return 0;
+}
