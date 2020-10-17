61171be5f7fe37d36814f9cf057d7ea04deea266
Musthafa Faishal <72973311+Musshal@users.noreply.github.com>
2020-10-17 17:24:42 +0700

    Add fruit_bowl.cpp
    Solved

---
+++ easy/fruit_bowl.cpp
@@ -0,0 +1,18 @@
+/* problem solver: musshal */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  /* dictionary */
+  int fruit, apple, pie;
+
+  /* algorithm */
+  cin >> fruit;
+  apple = fruit/2;
+  pie = apple/3;
+  cout << pie;
+
+  return 0;
+}
